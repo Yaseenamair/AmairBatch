6e11ac3244e1cf90bacd4021189c7677e7b5f856
sanjeevareddy91 <gsanjeevreddy91@gmail.com>
2019-03-24 07:29:14 +0530

    Day1 Files

---
+++ keywords.py
@@ -0,0 +1,36 @@
+# Keywords -- Words whose functionlity already assigned...
+
+# Module -- every python file 
+
+# import keyword
+
+# print(keyword.kwlist)
+
+# print(len(keyword.kwlist))
+
+
+# Variables or Identifiers::
+
+# Definition -- Memory Location in the system..
+
+# a = 17
+# b = 17
+
+# print(id(a))
+# print(id(b))
+
+# a&=43
+# b$=67
+ 
+# print(a&)
+# print(b$)
+
+
+#Rules to define variable or Identifier:
+	#1) A variable can contain lower,upper and numbers....(aA1,Aa1,A1a,a,A)
+	#2) A varible can contains number but should not start with number(Not aaccepted:  --1Aa,)
+	#3) Keywords cannot be taken as variables...
+	#4) Variable should not taken any special symbols except underscore(_)..(ex:- a_,A_,_1,).
+	#5) Single under score can also be taken as variables..(_=54)...
+
+
